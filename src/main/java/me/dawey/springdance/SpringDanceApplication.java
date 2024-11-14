45bdcd7ea471c5554bce805552c280e35d8c4ce2
DaweY1108 <vargadavidzsolt@gmail.com>
2024-11-14 12:46:15 +0100

    Initial commit

---
+++ src/main/java/me/dawey/springdance/SpringDanceApplication.java
@@ -0,0 +1,13 @@
+package me.dawey.springdance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/dawey/springdance/SpringDanceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dawey.springdance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
